fc1aaaf34bb3b0391b6fd2b18f0211ffce69541d
ctomera <clement.tomera.smb@gmail.com>
2026-02-05 10:40:27 +0100

    step3

--- Funky_Food_TOMERA_Clement/src/App.jsx
+++ Funky_Food_TOMERA_Clement/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
 import "./style.css";
 import PlatCard from "./components/PlatCard.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [dataPlats, setDataPlats] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function fetchData() {
+    setLoading(true);
+    fetch("https://dummyjson.com/recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        setDataPlats(data.recipes);
+        setLoading(false);
+      });
+  }
 
   return (
     <>
@@ -21,84 +36,28 @@ function App() {
                 Des plats sélectionnés qui vont vous époustoufler ! 🌈✨
               </p>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                <PlatCard
-                  id="1"
-                  titre="Pizza Margherita Classique"
-                  img="https://cdn.dummyjson.com/recipe-images/1.webp"
-                  categorie="Italienne"
-                  kcal="300"
-                  tpsPreparation="35"
-                  nbPortions="4"
-                  tags={["Pizza", "Italienne"]}
-                  note="4.6"
-                  nbAvis="98"
-                  difficulte="Facile"
-                ></PlatCard>
-                <PlatCard
-                  id="2"
-                  titre="Tacos Épicés Fiesta"
-                  img="https://cdn.dummyjson.com/recipe-images/7.webp"
-                  categorie="Mexicaine"
-                  kcal="380"
-                  tpsPreparation="35"
-                  nbPortions="6"
-                  tags={["Tacos", "Mexicaine", "Épicé"]}
-                  note="4.8"
-                  nbAvis="142"
-                  difficulte="Facile"
-                ></PlatCard>
-                <PlatCard
-                  id="3"
-                  titre="Buddha Bowl Arc-en-Ciel"
-                  img="https://cdn.dummyjson.com/recipe-images/2.webp"
-                  categorie="Végan"
-                  kcal="380"
-                  tpsPreparation="50"
-                  nbPortions="2"
-                  tags={["Sain", "Végan", "Bowl"]}
-                  note="4.7"
-                  nbAvis="87"
-                  difficulte="Moyen"
-                ></PlatCard>
-                <PlatCard
-                  id="4"
-                  titre="Spaces cookies"
-                  img="https://cdn.dummyjson.com/recipe-images/3.webp"
-                  categorie="Gourmand"
-                  kcal="450"
-                  tpsPreparation="50"
-                  nbPortions="2"
-                  tags={["Gourmand", "Festif", "Détente"]}
-                  note="4.7"
-                  nbAvis="87"
-                  difficulte="Moyen"
-                ></PlatCard>
-                <PlatCard
-                  id="5"
-                  titre="Saumon Grillé Groovy"
-                  img="https://cdn.dummyjson.com/recipe-images/5.webp"
-                  categorie="Gourmand"
-                  kcal="340"
-                  tpsPreparation="50"
-                  nbPortions="2"
-                  tags={["Saumon", "Sain", "Grillé"]}
-                  note="4.5"
-                  nbAvis="76"
-                  difficulte="Facile"
-                ></PlatCard>
-                <PlatCard
-                  id="6"
-                  titre="Pâtes Primavera Psychédéliques"
-                  img="https://cdn.dummyjson.com/recipe-images/6.webp"
-                  categorie="Italienne"
-                  kcal="380"
-                  tpsPreparation="50"
-                  nbPortions="2"
-                  tags={["Pâtes", "Végé", "Italienne"]}
-                  note="4.4"
-                  nbAvis="65"
-                  difficulte="Facile"
-                ></PlatCard>
+                {loading ? (
+                  <div>Chargement</div>
+                ) : (
+                  dataPlats.map((plat) => (
+                    <PlatCard
+                      key={plat.id}
+                      id={plat.id}
+                      titre={plat.name}
+                      img={plat.image}
+                      categorie={plat.cuisine}
+                      kcal={plat.caloriesPerServing}
+                      tpsPreparation={
+                        plat.prepTimeMinutes + plat.cookTimeMinutes
+                      }
+                      nbPortions={plat.servings}
+                      tags={plat.tags}
+                      note={plat.rating}
+                      nbAvis={plat.reviewCount}
+                      difficulte={plat.difficulty}
+                    ></PlatCard>
+                  ))
+                )}
               </div>
             </div>
           </div>
